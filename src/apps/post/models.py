0d128b5111637708841c5359e17cb0a17dcd64e0
Amina <tynybekovaamina@gmail.com>
2023-06-24 17:51:09 +0600

    created new models

--- src/apps/post/models.py
+++ src/apps/post/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+from src.apps.account.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField("Название", max_length=255)
+    description = models.TextField("Описание")
+    image = models.ImageField("Картинка", upload_to="post/image/")
+
+    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    updated_at = models.DateTimeField("Время добавления", auto_now=True)
+    
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+    def __str__(self):
+        return self.author.username
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comments = models.TextField()
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+
+    def __str__(self):
+        return self.author.username
+       
