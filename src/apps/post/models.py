40e2ddef3a10d968b1721dc42e82ceff02512527
Sultan <sultanbek9899@gmail.com>
2023-06-24 19:04:08 +0600

    add new related_names

--- src/apps/post/models.py
+++ src/apps/post/models.py
@@ -3,34 +3,35 @@ from django.db import models
 # Create your models here.
 from src.apps.account.models import User
 
-
+# related_name  u
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     description = models.TextField("Описание")
     image = models.ImageField("Картинка", upload_to="post/image/")
 
-    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
     updated_at = models.DateTimeField("Время добавления", auto_now=True)
-    
+    is_archived = models.BooleanField("Архивирован", default=False)
+
     class Meta:
         verbose_name = "Пост"
         verbose_name_plural = "Посты"
 
     def __str__(self):
-        return self.author.username
+        return f"#{self.id}-{self.description[:50]}"
     
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+class Comment(models.Model): 
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     comments = models.TextField()
-    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
 
     class Meta:
         verbose_name = "Комментарий"
         verbose_name_plural = "Комментарии"
 
     def __str__(self):
-        return self.author.username
+        return self.comments
        
 
 
