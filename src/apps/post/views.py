b94e2f38570a9f4bed94a6f992632d1f96f2e257
Jager - ip340 <baisalovdastan1505052@gmail.com>
2023-06-28 01:24:01 +0600

    first commit/Task #2

--- src/apps/post/views.py
+++ src/apps/post/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from .forms import PostForm
 
 from django.views.generic import TemplateView
 # Create your views here.
@@ -6,3 +7,10 @@ from django.views.generic import TemplateView
 class IndexPageView(TemplateView):
     template_name = "index.html"
 
+def create_post(request):
+    if request.method == 'Пост':
+        form = PostForm(request.POST, request.FILES)
+        form.save()            
+    else:
+        form = PostForm()
+    return render(request, 'create_post.html', {'form': form})
