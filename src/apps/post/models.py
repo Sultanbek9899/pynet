d3fea21b964a4e5c725f2773eae34179e9259d2a
ilgamkhisam <ilgamkhisam@gmail.com>
2023-07-01 18:40:54 +0600

    update_git_ignore

--- src/apps/post/models.py
+++ src/apps/post/models.py
@@ -7,7 +7,7 @@ from src.apps.account.models import User
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     description = models.TextField("Пост") 
-    image = models.ImageField("Картинка", upload_to="post/image/", blank=False, null=True)
+    image = models.ImageField("Картинка", upload_to="post/image/", blank=True, null=True)
 
     created_at = models.DateTimeField("Дата создания", auto_now_add=True)
     updated_at = models.DateTimeField("Время добавления", auto_now=True)
