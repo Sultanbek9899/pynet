915ed41cf7937d3d10a47b8d298a96fee809e867
ilgamkhisam <ilgamkhisam@gmail.com>
2023-07-29 18:40:37 +0600

    line

--- src/apps/api/serializers.py
+++ src/apps/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 
 from src.apps.post.models import Post
+from src.apps.account.models import User
 
 class PostSerializer(serializers.ModelSerializer):
     
@@ -15,3 +16,15 @@ class PostCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ["description", "image"]
+
+class UserDetaileView(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+class UserSeachView(serializers.ModelSerializer):
+
+        class Meta:
+            model = User
+            fields = "__all__"
