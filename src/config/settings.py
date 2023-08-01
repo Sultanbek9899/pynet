149221f6d24bd635b64f0315b2c6449b4bfada0c
dasifue <dasifue@gmail.com>
2023-08-01 19:18:36 +0600

    connected to PSQL

--- src/config/settings.py
+++ src/config/settings.py
@@ -89,8 +89,12 @@ AUTH_USER_MODEL = "account.User"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pynet',
+        'USER': 'pynet_admin',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
